f0e8db8fbe1711561dd7fb4a3b77bfba7979ba82
Gerardo Cianciulli <gerardo.cianciullu@gmail.com>
2026-04-20 10:40:59 -0400

    update data structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
-import ProductPreviewCard from './components/ProductPreviewCard'
-import './App.css'
-import desktopImage from './assets/image-product-desktop.jpg'
-import mobileImage from './assets/image-product-mobile.jpg'
+import ProductPreviewCard from "./components/ProductPreviewCard";
+import "./App.css";
+import desktopImage from "./assets/image-product-desktop.jpg";
+import mobileImage from "./assets/image-product-mobile.jpg";
+
+const data = {
+  type: "Perfume",
+  title: "Gabrielle Essence Eau De Parfum",
+  description:
+    "A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.",
+  salePrice: 149.99,
+  price: 169.99,
+  desktopImage: desktopImage,
+  mobileImage: mobileImage,
+};
 
 function App() {
   return (
     <main>
       <h1 className="bold">Product Preview Card Component</h1>
-      <ProductPreviewCard type="Perfume" title="Gabrielle Essence Eau De Parfum" description="A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL."
-        salePrice="$149.99" price="$169.99" desktopImage={desktopImage} mobileImage={mobileImage} />
+      <ProductPreviewCard
+        type={data.type}
+        title={data.title}
+        description={data.description}
+        salePrice={data.salePrice}
+        price={data.price}
+        desktopImage={data.desktopImage}
+        mobileImage={data.mobileImage}
+      />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/ProductPreviewCard.tsx
+++ src/components/ProductPreviewCard.tsx
@@ -1,44 +1,51 @@
-import Button from "./Button.tsx"
-import CartIcon from "./CartIcon.tsx"
+import Button from "./Button.tsx";
+import CartIcon from "./CartIcon.tsx";
 
 type ProductPreviewCardProps = {
-    title: string;
-    type: string;
-    description: string;
-    salePrice: string;
-    price: string;
-    desktopImage: string;
-    mobileImage: string;
-}
+  title: string;
+  type: string;
+  description: string;
+  salePrice: number;
+  price: number;
+  desktopImage: string;
+  mobileImage: string;
+};
 
-const ProductPreviewCard = ({ title, type, description, salePrice, price, desktopImage:Desktop, mobileImage:Mobile}: ProductPreviewCardProps) => {
-    return (
-        <section className="product-preview-card" aria-label={title}>
-            <div className="side" >
-                <img className="desktop-image" src={Desktop} alt={title} />
-                <img className="mobile-image" src={Mobile} alt={title} />
-            </div>
+const ProductPreviewCard = ({
+  title,
+  type,
+  description,
+  salePrice,
+  price,
+  desktopImage: Desktop,
+  mobileImage: Mobile,
+}: ProductPreviewCardProps) => {
+  return (
+    <section className="product-preview-card" aria-label={title}>
+      <div className="side">
+        <img className="desktop-image" src={Desktop} alt={title} />
+        <img className="mobile-image" src={Mobile} alt={title} />
+      </div>
 
-            <div className="side">
-                <div className="product-details">
-                    <div className="product-header">
-                        <h2 className="product-type">{type}</h2>
-                        <h3 className="product-name bold">{title}</h3>
+      <div className="side">
+        <div className="product-details">
+          <div className="product-header">
+            <h2 className="product-type">{type}</h2>
+            <h3 className="product-name bold">{title}</h3>
 
-                        <p className="product-description">
-                            {description}
-                        </p>
-                    </div>
+            <p className="product-description">{description}</p>
+          </div>
 
-                    <div className="cost-widget">
-                        <p className="sale-price bold">{salePrice}</p>
-                        <p className="price">{price}</p>
-                    </div>
+          <div className="cost-widget">
+            <p className="sale-price bold">${salePrice}</p>
+            <p className="price">${price}</p>
+          </div>
 
-                    <Button text="Add to Cart" icon={CartIcon} />
-                </div>
-            </div>
-        </section>)
-}
+          <Button text="Add to Cart" icon={CartIcon} />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default ProductPreviewCard
+export default ProductPreviewCard;
